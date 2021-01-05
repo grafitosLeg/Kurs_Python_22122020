f174970acb167032e9fe8bcf0ea2d955339ebe8a
grafitosLeg <wawrzyniakrafa74@gmail.com>
2021-01-05 17:00:53 +0100

    Update files with function, add some dict feature

--- Kurs3_PA_KA_NO_z_funkcja_slownikiem.py
+++ Kurs3_PA_KA_NO_z_funkcja_slownikiem.py
@@ -1,5 +1,8 @@
 #Gra papier, nozyczki, kamien z użyciem funcji i słownika
 
+from time import sleep
+print ("Gra Papier, Kamien, Nożyczki")
+sleep(3)
 
 gracz1_wynik = 0
 gracz2_wynik = 0
@@ -9,6 +12,7 @@ opcje = ['kamien', 'papier', 'nozyczki']
 #gra jest rozgrywana do 3 wygranych rozgrywek przez dowolnego gracza
 #włączona logika poprawności wprowadzonego inputa
 
+#funkcja pobierająca wybór od graczy 
 def pobierz_wybor(gracz):
     while True:
         wybor_Gracz = input(f'{gracz} podaj wybor: ')
@@ -16,21 +20,24 @@ def pobierz_wybor(gracz):
              return wybor_Gracz
 
 
+#funkcja sprawdzająca wyniki
 def sprawdz_wyniki (wybor_Gracz1, wybor_Gracz2):
     if wybor_Gracz1 == wybor_Gracz2:
       print('REMIS')
       return 0
 
     
-
+#słownik z którego pobierane są klucze z tupli 
     wynik = {
         ('papier','kamien') :1,
         ('kamien','nozyczki') :1,
         ('nozyczki','papier') :1 
     }
 
+#return wynik z tupli dostępnych w słowników, w przypadku braku dostępnych zwróć wartość -1 
     return wynik.get ((wybor_Gracz1,wybor_Gracz2),  -1)
 
+#graj dopóki któryś z graczy nie osiągnie 3 wygranych ruchów
 while gracz1_wynik != 3 and gracz2_wynik != 3:
     wybor_Gracz1 = pobierz_wybor ('Gracz1')
     wybor_Gracz2 = pobierz_wybor ('Gracz2')
--- Kurs3_pap_noz_kam_z_funkcja.py
+++ Kurs3_pap_noz_kam_z_funkcja.py
@@ -1,44 +1,44 @@
 #Gra papier, nozyczki, kamien z użyciem funcji - kod z głowy
 
 #definicja graczy  - ustalamy licznik na 0
-
 gracz1_wynik = 0
 gracz2_wynik = 0
 
+
 #definicja opcji z których gracze będą korzystać przy wyborze
 
-opcje = ['kamien', 'nozyczki','papier']
+opcje = ['papier','kamien','nozyczki']
 
 #definicja funkcji pobierz wybór gracza 
-
 def pobierz_wybor (gracz):
-   while True:
-       wybor_Gracz = input (f'{gracz} podaj wybor:   ')
-       if wybor_Gracz in opcje:
-          return wybor_Gracz 
+    while True:
+        wybor_gracz = input (f'{gracz} podaj wybór:   ')
+        if wybor_gracz in opcje:
+            return wybor_gracz
+
 
 
 #definicja funkcji w której sprawdzamy wyniki 
-
 def sprawdz_wyniki (wybor_gracz1, wybor_gracz2):
     if wybor_gracz1 == 'papier' and wybor_gracz2 == 'kamien'\
-    or wybor_gracz1 == 'nozyczki' and wybor_gracz2 == 'papier'\
-    or wybor_gracz1 == 'kamien' and wybor_gracz2 == 'nozyczki':
-        print ('Wygrał Gracz 1')
-        return 1 
-    
+    or wybor_gracz1 == 'kamien' and wybor_gracz2 == 'nozyczki'\
+    or wybor_gracz1 == 'nozyczki' and wybor_gracz2 == 'papier':
+        print('Gracz1 wygrał')
+        return 1
+
+
     elif wybor_gracz1 == wybor_gracz2:
-        print ('REMIS')
-        return 0    
+         print ('REMIS')
+         return 0
+
     else:
-         print ('Wygrał Gracz 2')
-         return -1
-    
+        print ('Gracz2 wygrał')
+        return -1
 
 #uruchamiamy funkcje
 #gramy dopóki któryś z graczy nie wygra 3razy
 
-while gracz1_wynik != 3 and gracz2_wynik != 3:
+while gracz1_wynik != 3 and gracz2_wynik !=3:
     wybor_gracz1 = pobierz_wybor ('Gracz1')
     wybor_gracz2 = pobierz_wybor ('Gracz2')
     wynik = sprawdz_wyniki (wybor_gracz1, wybor_gracz2)
@@ -50,10 +50,14 @@ while gracz1_wynik != 3 and gracz2_wynik != 3:
        gracz1_wynik += 1
 
     elif wynik == -1:
-       gracz2_wynik += 1
-
-
+         gracz2_wynik += 1
+  
+#kto wygrał?
 if gracz1_wynik > gracz2_wynik:
-    print ('Całą grę wygrał Gracz1')
+    print('Całą grę wygrał Gracz1')
+
 else:
-     print('Całą grę wygrał Gracz2')
+    print ('Całą grę wygrał Gracz2')
+
+ 
+
