4d3f1456bc5353b3686bb6c5e27325e900f1aa0d
grafitosLeg <wawrzyniakrafa74@gmail.com>
2021-01-21 18:11:18 +0100

    Update comment, doublecheck after git push fail

--- Kurs4_Kompas_kolekcji_tupla.py
+++ Kurs4_Kompas_kolekcji_tupla.py
@@ -7,19 +7,23 @@ lap_times = (
   80.84  
 )
 
-print (lap_times[0])
-################ 1 wersja
-#print (lap_times[1: ]) SLICE od 1: do końca
+print (lap_times [0])
+#1st wersja SLICE + pętla
+#print (lap_times [1:])
 
-#for x in lap_times[1: ]:
-# print (x)
+#for x in lap_times:
+ #   print (x)
 
-####### 2 wersja
+
+#2nd wersja z pętlą i licznikiem
 #time_sum = 0
+
 #for lap_time in lap_times [1: ]:
-    #time_sum += lap_time
-#print (time_sum/ (len(lap_times) -1))
+#   print (lap_time)
+#   time_sum += lap_time
+#print (time_sum / (len (lap_times) -1))
 
-time_sum = sum (lap_times [1: ])
+#3rd wersja 
 
-print (time_sum/ (len(lap_times) -1))
+time_sum = sum (lap_times [1:])
+print (time_sum / (len(lap_times) -1))
